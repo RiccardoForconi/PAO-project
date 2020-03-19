a234d43b59d9cf4278e3c606b4f37a5c33199363
RiccardoForconi <61538861+RiccardoForconi@users.noreply.github.com>
2020-03-19 18:01:01 +0100

    Update metadati.java
    versione con commenti

--- progettoSpringBootApp/src/main/java/it/univpm/progettoSpringBootApp/model/metadati.java
+++ progettoSpringBootApp/src/main/java/it/univpm/progettoSpringBootApp/model/metadati.java
@@ -1,11 +1,30 @@
 package it.univpm.progettoSpringBootApp.model;
 
-
+/**
+     * Classe metadati che definisce i metadati relativi alle 
+     * classi Farmacia
+     */
 public class metadati {
+	/**
+     * nome che rappresenta il nominativo all'interno del dataset
+     */
 	private String sourceField;
+	/**
+     * nome che rappresenta il nominativo della variabile nel programma
+     */
 	private String aliases;
+	/**
+     * nome che rappresenta il tipo della variabile
+     */
 	private String tipo;
 	
+	/**
+     * Costruttore che prende i dati e li inserisce in un oggetto
+     * metadati
+     * @param s: stringa inserita in sourceField
+     * @param t: stringa inserita in tipo
+     * @param a: stringa inserita in aliases
+     */
 	public metadati(String s, String t, String a) {
 		this.sourceField = s;
 		this.tipo = t;
